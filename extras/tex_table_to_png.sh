0f097dec9fecc05d7f86e789bae12d2c5e2cc68c
Aki Nishimura <akihiko4@g.ucla.edu>
2020-11-08 13:33:14 -0500

    Drop colorspace flag instead of using PNG24
    not sure why it works, but using PNG24 makes non-math texts look weird

--- extras/tex_table_to_png.sh
+++ extras/tex_table_to_png.sh
@@ -4,5 +4,5 @@ for file in $(ls {*table.tex,*table_[0-9].tex})
 do
   pdflatex $file
   name=${file%.tex}
-  magick convert -density 300 -units pixelsperinch $name.pdf -colorspace RGB -trim PNG24:$name.png
+  magick convert -density 300 -units pixelsperinch $name.pdf -trim $name.png
 done
