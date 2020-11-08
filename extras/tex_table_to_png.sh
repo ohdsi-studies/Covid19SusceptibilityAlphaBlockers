ef514fb29b74f75ec07da1d5ae98ea947df044f3
Aki Nishimura <akihiko4@g.ucla.edu>
2020-11-08 11:52:43 -0500

    Use PNG24 to make sure gray color comes out

--- extras/tex_table_to_png.sh
+++ extras/tex_table_to_png.sh
@@ -4,5 +4,5 @@ for file in $(ls {*table.tex,*table_[0-9].tex})
 do
   pdflatex $file
   name=${file%.tex}
-  magick convert -density 300 -units pixelsperinch $name.pdf -colorspace RGB -trim $name.png
+  magick convert -density 300 -units pixelsperinch $name.pdf -colorspace RGB -trim PNG24:$name.png
 done
